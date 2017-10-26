be44f99e0b7c03fda7a5c180b3a3fc4f09b6241c
admin <slau2012@gmail.com>
2017-10-26 14:38:30 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DataTableModule} from 'primeng/components/datatable/datatable';
+import {DialogModule} from 'primeng/components/dialog/dialog';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {AppComponent} from './app.component';
+import {JumbotronComponent} from './jumbotron/jumbotron.component';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {FooterComponent} from './footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {CardListComponent} from './card-list/card-list.component';
+import {CardContentDirective, CardDirective} from './card-list/card.component';
+import {FragmentDirective} from './shared/fragment.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JumbotronComponent,
+    CardDirective,
+    CardListComponent,
+    CardContentDirective,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    FooterComponent,
+    FragmentDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    DataTableModule,
+    DialogModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
